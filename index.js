efd60e1bcf5b80f79d70da12efd0404b1570c8ab
Kyle Leonard <kdleonard@icloud.com>
2020-04-20 19:13:55 -0500

    removed env

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const server = require("./server");
 
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
